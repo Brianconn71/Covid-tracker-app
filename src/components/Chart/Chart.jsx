9ec928cb0516a19f66e8fa1c06ba1d11c682eb91
Brian Connolly <Brian.connolly71@gmail.com>
2022-01-23 12:54:16 +0000

    added file and folder structure

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,10 @@
+//  Functional Component
+import React from 'react';
+
+const Charts = () => {
+    return(
+        <h1>Charts</h1>
+    )
+}
+
+export default Charts;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,10 @@
+//  Functional Component
+import React from 'react';
+
+const CountryPicker = () => {
+    return(
+        <h1>CountryPicker</h1>
+    )
+}
+
+export default CountryPicker;
