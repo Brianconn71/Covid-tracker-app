de8bf6062ae578717b3ff5c535713f6a818f9b70
Brian Connolly <Brian.connolly71@gmail.com>
2022-01-31 20:50:25 +0000

    add: added react charts import and working on charts

--- src/api/index.js
+++ src/api/index.js
@@ -16,3 +16,13 @@ export const fetchData = async () => {
 
     }
 }
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        console.log(data);
+    } catch(err){
+
+    }
+}
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,30 @@
 //  Functional Component
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchAPI();
+    });
+
+    const lineChart = (
+        <Line
+            data={{
+                
+            }}
+        />
+
+    );
+
     return(
         <h1>Charts</h1>
     )
